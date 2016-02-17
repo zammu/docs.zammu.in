ddbfa144cfec7a8c4fa87958f130990dd00e833c
Zammu Web <zammuweb@zammu.in>
2016-02-17 17:12:44 +0530

    96bb1382-bbe3-4ba6-bc46-c7efb7604d1b

--- js/theme.js
+++ js/theme.js
@@ -1,131 +1,55 @@
-function toggleCurrent (elem) {
-    var parent_li = elem.closest('li');
-    parent_li.siblings('li.current').removeClass('current');
-    parent_li.siblings().find('li.current').removeClass('current');
-    parent_li.find('> ul li.current').removeClass('current');
-    parent_li.toggleClass('current');
-}
+$( document ).ready(function() {
 
-$(document).ready(function() {
     // Shift nav in mobile when clicking the menu.
     $(document).on('click', "[data-toggle='wy-nav-top']", function() {
-        $("[data-toggle='wy-nav-shift']").toggleClass("shift");
-        $("[data-toggle='rst-versions']").toggleClass("shift");
+      $("[data-toggle='wy-nav-shift']").toggleClass("shift");
+      $("[data-toggle='rst-versions']").toggleClass("shift");
     });
-    // Nav menu link click operations
+
+    // Close menu when you click a link.
     $(document).on('click', ".wy-menu-vertical .current ul li a", function() {
-        var target = $(this);
-        // Close menu when you click a link.
-        $("[data-toggle='wy-nav-shift']").removeClass("shift");
-        $("[data-toggle='rst-versions']").toggleClass("shift");
-        // Handle dynamic display of l3 and l4 nav lists
-        toggleCurrent(target);
-        if (typeof(window.SphinxRtdTheme) != 'undefined') {
-            window.SphinxRtdTheme.StickyNav.hashChange();
-        }
+      $("[data-toggle='wy-nav-shift']").removeClass("shift");
+      $("[data-toggle='rst-versions']").toggleClass("shift");
     });
+
     $(document).on('click', "[data-toggle='rst-current-version']", function() {
-        $("[data-toggle='rst-versions']").toggleClass("shift-up");
+      $("[data-toggle='rst-versions']").toggleClass("shift-up");
     });
+
     // Make tables responsive
     $("table.docutils:not(.field-list)").wrap("<div class='wy-table-responsive'></div>");
 
-    // Add expand links to all parents of nested ul
-    $('.wy-menu-vertical ul').not('.simple').siblings('a').each(function () {
-        var link = $(this);
-            expand = $('<span class="toctree-expand"></span>');
-        expand.on('click', function (ev) {
-            toggleCurrent(link);
-            ev.stopPropagation();
-            return false;
-        });
-        link.prepend(expand);
-    });
+    hljs.initHighlightingOnLoad();
+
+    $('table').addClass('docutils');
 });
 
-// Sphinx theme state
 window.SphinxRtdTheme = (function (jquery) {
     var stickyNav = (function () {
         var navBar,
             win,
-            winScroll = false,
-            winResize = false,
-            linkScroll = false,
-            winPosition = 0,
-            winHeight,
-            docHeight,
+            stickyNavCssClass = 'stickynav',
+            applyStickNav = function () {
+                if (navBar.height() <= win.height()) {
+                    navBar.addClass(stickyNavCssClass);
+                } else {
+                    navBar.removeClass(stickyNavCssClass);
+                }
+            },
             enable = function () {
-                init();
-                reset();
-                win.on('hashchange', reset);
-
-                // Set scroll monitor
-                win.on('scroll', function () {
-                    if (!linkScroll) {
-                        winScroll = true;
-                    }
-                });
-                setInterval(function () { if (winScroll) scroll(); }, 25);
-
-                // Set resize monitor
-                win.on('resize', function () {
-                    winResize = true;
-                });
-                setInterval(function () { if (winResize) resize(); }, 25);
-                resize();
+                applyStickNav();
+                win.on('resize', applyStickNav);
             },
             init = function () {
-                navBar = jquery('div.wy-side-scroll:first');
-                win = jquery(window);
-            },
-            reset = function () {
-                // Get anchor from URL and open up nested nav
-                var anchor = encodeURI(window.location.hash);
-                if (anchor) {
-                    try {
-                        var link = $('.wy-menu-vertical')
-                            .find('[href="' + anchor + '"]');
-                        $('.wy-menu-vertical li.toctree-l1 li.current')
-                            .removeClass('current');
-                        link.closest('li.toctree-l2').addClass('current');
-                        link.closest('li.toctree-l3').addClass('current');
-                        link.closest('li.toctree-l4').addClass('current');
-                    }
-                    catch (err) {
-                        console.log("Error expanding nav for anchor", err);
-                    }
-                }
-            },
-            scroll = function () {
-                winScroll = false;
-                var newWinPosition = win.scrollTop(),
-                    winBottom = newWinPosition + winHeight,
-                    navPosition = navBar.scrollTop(),
-                    newNavPosition = navPosition + (newWinPosition - winPosition);
-                if (newWinPosition < 0 || winBottom > docHeight) {
-                    return;
-                }
-                navBar.scrollTop(newNavPosition);
-                winPosition = newWinPosition;
-            },
-            resize = function () {
-                winResize = false;
-                winHeight = win.height();
-                docHeight = $(document).height();
-            },
-            hashChange = function () {
-                linkScroll = true;
-                win.one('hashchange', function () {
-                    linkScroll = false;
-                });
+                navBar = jquery('nav.wy-nav-side:first');
+                win    = jquery(window);
             };
         jquery(init);
         return {
-            enable: enable,
-            hashChange: hashChange
+            enable : enable
         };
     }());
     return {
-        StickyNav: stickyNav
+        StickyNav : stickyNav
     };
 }($));
